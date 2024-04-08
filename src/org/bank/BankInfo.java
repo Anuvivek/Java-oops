6a002d69c8d64d8695e057c8c7e837ce0ec468c2
Anupriya <anupriyaa.business@gmail.com>
2024-04-08 17:30:19 +0530

    My Task

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class BankInfo{
+	
+	private void saving() {
+		System.out.println("Savimgs %6");
+	}
+	private void fixed() {
+		System.out.println("Fixed %7");
+	}
+	public void deposit() {
+		System.out.println("Deposit %8");	
+		}
+}
